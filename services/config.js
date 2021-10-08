1352fa9ba15d5441bcf1f271451919acaab7c33b
Henrique <rique.duarte@outlook.com.br>
2021-10-07 23:43:18 -0300

    changing db;

--- services/config.js
+++ services/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    // baseURL: 'https://api-studio-moreiras.herokuapp.com/'
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'https://api-studio-moreiras.herokuapp.com/'
+    // baseURL: 'http://localhost:5000/'
 })
